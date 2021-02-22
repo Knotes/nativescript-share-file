71f63ae26b5842e0ec4b1fb59cde4f94da778f14
Yizhao He <heyizhaobupt@gmail.com>
2021-02-21 19:17:01 -0800

    Fix file sharing for android 11

--- src/share-file.android.ts
+++ src/share-file.android.ts
@@ -5,13 +5,10 @@ export class ShareFile {
     if (args.path) {
       try {
         let intent = new android.content.Intent();
-        let map = android.webkit.MimeTypeMap.getSingleton();
-        let mimeType = map.getMimeTypeFromExtension(this.fileExtension(args.path));
-
         intent.addFlags(android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
         let uris = new java.util.ArrayList();
-        let uri = this._getUriForPath(args.path, '/' + this.fileName(args.path), Application.android.context);
+        let uri = this._getUriForPath(args.path);
         uris.add(uri);
         let builder = new android.os.StrictMode.VmPolicy.Builder();
         android.os.StrictMode.setVmPolicy(builder.build());
@@ -38,87 +35,13 @@ export class ShareFile {
   fileName(filename) {
     return filename.split('/').pop();
   }
-
-  _getUriForPath(path, fileName, ctx) {
-    if (path.indexOf("file:///") === 0) {
-      return this._getUriForAbsolutePath(path);
-    } else if (path.indexOf("file://") === 0) {
-      return this._getUriForAssetPath(path, fileName, ctx);
-    } else if (path.indexOf("base64:") === 0) {
-      return this._getUriForBase64Content(path, fileName, ctx);
-    } else {
-      if (path.indexOf(ctx.getPackageName()) > -1) {
-        return this._getUriForAssetPath(path, fileName, ctx);
-      } else {
-        return this._getUriForAbsolutePath(path);
-      }
-    }
+  _getUriForPath(path) {
+    var file = new java.io.File(path);
+    return androidx.core.content.FileProvider.getUriForFile(
+        Application.android.foregroundActivity ||
+        Application.android.startActivity,
+        Application.android.packageName + ".provider",
+        file
+    );
   }
-  _getUriForAbsolutePath(path) {
-    let absPath = path.replace("file://", "");
-    let file = new java.io.File(absPath);
-    if (!file.exists()) {
-      console.log("File not found: " + file.getAbsolutePath());
-      return null;
-    } else {
-      return android.net.Uri.fromFile(file);
-    }
-  }
-  _getUriForAssetPath(path, fileName, ctx) {
-    path = path.replace("file://", "/");
-    if (!File.exists(path)) {
-      console.log("File does not exist: " + path);
-      return null;
-    }
-    let localFile = File.fromPath(path);
-    let localFileContents = localFile.readSync(function (e) { console.log(e); });
-    let cacheFileName = this._writeBytesToFile(ctx, fileName, localFileContents);
-    if (cacheFileName.indexOf("file://") === -1) {
-      cacheFileName = "file://" + cacheFileName;
-    }
-    return android.net.Uri.parse(cacheFileName);
-  }
-  _getUriForBase64Content(path, fileName, ctx) {
-    let resData = path.substring(path.indexOf("://") + 3);
-    let bytes;
-    try {
-      bytes = android.util.Base64.decode(resData, 0);
-    } catch (ex) {
-      console.log("Invalid Base64 string: " + resData);
-      return android.net.Uri.EMPTY;
-    }
-    let cacheFileName = this._writeBytesToFile(ctx, fileName, bytes);
-    return android.net.Uri.parse(cacheFileName);
-  }
-  _writeBytesToFile(ctx, fileName, contents) {
-    let dir = ctx.getExternalCacheDir();
-    if (dir === null) {
-      console.log("Missing external cache dir");
-      return null;
-    }
-    let storage = dir.toString() + "/filecomposer";
-    let cacheFileName = storage + "/" + fileName;
-    let toFile = File.fromPath(cacheFileName);
-    toFile.writeSync(contents, function (e) { console.log(e); });
-    if (cacheFileName.indexOf("file://") === -1) {
-      cacheFileName = "file://" + cacheFileName;
-    }
-    return cacheFileName;
-  }
-  _cleanAttachmentFolder() {
-    if (Application.android.context) {
-      let dir = Application.android.context.getExternalCacheDir();
-      let storage = dir.toString() + "/filecomposer";
-      let cacheFolder = Folder.fromPath(storage);
-      cacheFolder.clear();
-    }
-  }
-  toStringArray(arg) {
-    let arr = java.lang.reflect.Array.newInstance(java.lang.String.class, arg.length);
-    for (let i = 0; i < arg.length; i++) {
-      arr[i] = arg[i];
-    }
-    return arr;
-  }
-
 }
